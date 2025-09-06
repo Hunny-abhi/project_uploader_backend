3e94e1e598f39616a951500993db1c01d8967b01
Hunny-abhi <hunny.abhi6552@gmail.com>
2025-09-06 07:49:41 +0530

    vercal origin added

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://project-uploader-frontend.vercel.app",
+    ],
   })
 );
 
