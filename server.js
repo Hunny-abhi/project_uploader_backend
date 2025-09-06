f3c7f6b16480f1ffd5820ef588bd9c8e49f4bcbb
Hunny-abhi <hunny.abhi6552@gmail.com>
2025-09-06 07:52:58 +0530

    multiple cors origin added

--- server.js
+++ server.js
@@ -10,9 +10,22 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://project-uploader-frontend.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl etc)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 
